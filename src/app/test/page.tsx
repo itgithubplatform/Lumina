fe1ce848944cad86d555a160c0518697314b1b1e
Kushal Mondal <kushalmondal349@gmail.com>
2025-10-06 23:04:23 +0530

    ffmpeg and google service added

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,18 @@
 import { GoogleAi } from '@/lib/googleAi'
+import { HLSConverter } from '@/lib/HLSConvarter'
 import React from 'react'
 
 export default async function page() {
     const googleAi = GoogleAi.getInstance()
-    // const text = await googleAi.generateText("Explain the theory of relativity in simple terms.");
-    const text = await googleAi.generateTextToSpeech("Hello, this is a test of Google Text to Speech API. I hope you find this useful.", "public/output.mp3");
+  //  const path = await googleAi.generateSpeechToText("gs://lumina-tech/audio/1759767484946-output.mp3")
+  //  console.log(path);
+  const hlsConvarter = HLSConverter.getInstance()
+ const hlsPath = await hlsConvarter.extractAudio("public/1.mp4", "public/hls");
+ console.log(hlsPath);   
   return (
     <div>
         <h1 className='text-2xl font-bold'>Google AI Test</h1>
-        <p className='mt-4'>{text}</p>      
+        <p className='mt-4'></p>      
     </div>
   )
 }
--- src/lib/googleAi.ts
+++ src/lib/googleAi.ts
@@ -1,11 +1,17 @@
 import { VertexAI } from "@google-cloud/vertexai";
 import textToSpeech from "@google-cloud/text-to-speech";
 import { writeFile } from "fs/promises";
+import { Storage } from "@google-cloud/storage";
+import path from "path";
+import fs from "fs";
+import Speech from "@google-cloud/speech";
 
 export class GoogleAi {
     private static instance: GoogleAi;
     private vertex;
     private ttsClient;
+    private storageClient;
+    private SpeechClient;
     private constructor() {
         const projectID = process.env.GOOGLE_PROJECT_ID;
         if (!projectID) {
@@ -13,14 +19,15 @@ export class GoogleAi {
         }
         this.vertex = new VertexAI({
             project: projectID,
-            location: "asia-south1",
             googleAuthOptions: {
                 keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
             },
         }).getGenerativeModel({
-            model: "gemini-2.5-flash",
+            model: "gemini-2.5-flash-lite",
         });
         this.ttsClient = new textToSpeech.TextToSpeechClient();
+        this.storageClient = new Storage()
+        this.SpeechClient = new Speech.SpeechClient();
     }
     public static getInstance(): GoogleAi {
         if (!GoogleAi.instance) {
@@ -47,13 +54,12 @@ export class GoogleAi {
                 input: { text },
                 voice: {
                     "languageCode": "en-in",
-                    "modelName": "gemini-2.5-flash-tts",
-                    "name": "Aoede"
+                    "name": "en-IN-Wavenet-D"
                 },
                 audioConfig: {
                     audioEncoding: "MP3",
-                    speakingRate: 1.0,
-                    pitch: -5.0,
+                    speakingRate: 0.9,
+                    pitch: 1.0,
                 },
             })
             if (!response.audioContent) {
@@ -64,5 +70,78 @@ export class GoogleAi {
             throw new Error("Error generating speech: " + (error as Error)?.message);;
         }
     }
+    async generateSpeechToText(audioUrl: string, lang:"en"|"hi"|"bn" = "en") {
+    try {
+      const audio = { uri: audioUrl };
 
+      const [operation] = await this.SpeechClient.longRunningRecognize({
+        audio,
+         config:{
+        encoding: "MP3",
+        enableAutomaticPunctuation: false,
+        enableWordTimeOffsets: false,
+        sampleRateHertz: 44100,
+        languageCode: lang,
+        model: "default", 
+      }
+      });
+
+      const [response] = await operation.promise();
+
+      const results = response.results || [];
+
+      const fullTranscription = results
+        .map((result) => result.alternatives?.[0]?.transcript)
+        .join(" ");
+
+      const words = results
+        .flatMap((result) => result.alternatives?.[0]?.words || [])
+        .map((word) => ({
+          word: word.word,
+          startTime: parseFloat(word.startTime?.seconds?.toString() || "0") +
+                     (word.startTime?.nanos || 0) / 1e9,
+          endTime: parseFloat(word.endTime?.seconds?.toString() || "0") +
+                   (word.endTime?.nanos || 0) / 1e9,
+        }));
+
+      return {
+        transcription: fullTranscription.trim(),
+        words, 
+        detectedLanguage:
+          results[0]?.languageCode || "unknown", 
+      };
+    } catch (error) {
+      throw new Error(
+        "Error transcribing from URL: " + (error as Error).message
+      );
+    }
+  }
+    async uploadToCloudStorage(sourcePath: string, fileType: "hls" | "audio"): Promise<string> {
+        const bucketName = process.env.GOOGLE_CLOUD_STORAGE_BUCKET;
+        if (!bucketName) throw new Error("Missing bucket");
+
+        const bucket = this.storageClient.bucket(bucketName);
+
+        if (fileType === "hls") {
+            const files = fs.readdirSync(sourcePath);
+            const uploadId = Date.now();
+            await Promise.all(files.map(file => {
+                const filePath = path.join(sourcePath, file);
+                const ext = path.extname(file);
+                return bucket.upload(filePath, {
+                    destination: `hls/${uploadId}/${file}`,
+                    metadata: {
+                        cacheControl: ext === '.m3u8' ? "public, max-age=2" : "public, max-age=31536000"
+                    }
+                });
+            }));
+            return `https://storage.googleapis.com/${bucketName}/hls/${uploadId}/master.m3u8`;
+        }
+        const newFileName = `${Date.now()}-${path.basename(sourcePath)}`
+        const [file] = await bucket.upload(sourcePath, {
+            destination: `audio/${newFileName}`,
+            metadata: { cacheControl: "public, max-age=31536000" }
+        });
+        return file.publicUrl();
+    }
 }
